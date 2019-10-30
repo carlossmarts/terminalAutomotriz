cc0a6436323bb86f10907c45e0d0ce14c805bce1
MVelasquez98 <43156596+MVelasquez98@users.noreply.github.com>
2019-10-30 00:27:58 -0300

    Reporte 11 actualizado

--- archivos SQL/reporte 11.sql
+++ archivos SQL/reporte 11.sql
@@ -5,7 +5,6 @@ use proyectoBD;
 encuentra finalizado, y si no esta terminado, indicar en qué estación se encuentra.
 **/
 
-drop function if exists existePedido;
 -- Funcion para saber si el pedido Existe, y realizar validaciones cuando se ejecute el procedimiento vehiculosFinalizados
 delimiter //
 CREATE function existePedido(
@@ -21,7 +20,7 @@ delimiter ;
 drop procedure if exists vehiculosFinalizados;
 delimiter //
 -- Entrada: Numero de Pedido
--- Salida: Lista Automovil: nroChasis, terminado (1=si o 0=no), estacion (compuesta por la Tarea y la Linea de Monataje)
+-- Salida: Lista Automovil: nroChasis,estado (En proceso o Terminado) estacion (compuesta por la Tarea y la Linea de Monataje)
 -- La estacion solo se indica si el Automovil no esta terminado
 CREATE PROCEDURE vehiculosFinalizados(
 	in _nroPedido int,
@@ -29,15 +28,43 @@ CREATE PROCEDURE vehiculosFinalizados(
     out _valorRetorno int)
 BEGIN
 if existePedido(_nroPedido)<>0 then
--- En Caso de que Exista el Pedido, se listan los automoviles con ese pedido que esten en las estaciones de trabajo
-	select a.nroChasis, a.idPedido , (select fechaTerminado is not null)as terminado, e.idTarea as tarea, e.idLineaDeMontaje as linea
-	from automovil a inner join automovilxestacion e on e.nroChasis = a.nroChasis
-			where idPedido = _nroPedido
-			UNION 
--- Se agregan al Listado los Automoviles que estan terminados
-	SELECT  a.nroChasis, a.idPedido , (select fechaTerminado is not null)as terminado, 0 as tarea, 0 as linea 
-		from automovil a 
-			WHERE NOT exists(SELECT * FROM automovilxestacion e1 WHERE e1.nroChasis = a.nroChasis) ;
+SELECT 
+    a.nroChasis,
+    a.idPedido,
+    'En proceso' AS estado,
+    e.idTarea AS tarea,
+    e.idLineaDeMontaje AS linea
+FROM
+    Automovil a
+        INNER JOIN
+    automovilxestacion e ON e.nroChasis = a.nroChasis
+WHERE
+    idPedido = _nroPedido
+    and e.horaIngreso = (SELECT 
+            MAX(horaIngreso)
+        FROM
+            automovilxestacion axe
+        WHERE
+            a.idPedido = _nroPedido AND horaEgreso IS NULL AND e.nroChasis = axe.nroChasis)
+UNION SELECT 
+    a.nroChasis,
+    a.idPedido,
+    'Terminado' AS estado,
+    0 AS tarea,
+    0 AS linea
+FROM
+    Automovil a
+WHERE
+    a.fechaTerminado is not null
+    and idPedido=_nroPedido
+UNION SELECT 
+	a.nroChasis,
+    a.idPedido,
+    'Pendiente' AS estado,
+    0 AS tarea,
+    0 AS linea
+FROM Automovil a
+WHERE NOT exists (SELECT NULL FROM automovilXestacion e WHERE e.nroChasis = a.nroChasis) AND idPedido=_nroPedido ;
 else 
 	select 'error, no existePedido' into _msjRetorno;
 	select -1 into _valorRetorno;
@@ -45,16 +72,21 @@ end if;
 END 
 //
 delimiter ;
-
-insert into automovil values(1234, 1, 1, null,null,now());
-insert into automovil values(4567, 1, 1, null,null,now());
-insert into automovil values(789, 1, 1, null,null,null);
-insert into automovilxestacion values(789,1,1,now(),null,null,null);
-SELECT * FROM automovil;
-SELECT * FROM automovilxestacion;
+-- PRUEBA REPORTE 11
 SELECT @msj, @valoRetorno;
+call procesarPedido (1,@m,@v);
+call iniciarMontaje(1,@msj,@valor);
+call iniciarMontaje(3,@msj,@valor);
+call iniciarMontaje(2,@msj,@valor);
+call iniciarMontaje(6,@msj,@valor);
+call continuarMontaje(1,@m,@v);
+call continuarMontaje(1,@m,@v);
+call continuarMontaje(1,@m,@v);
+call continuarMontaje(1,@m,@v);
+call continuarMontaje(1,@m,@v);
+call continuarMontaje(1,@m,@v);
+call continuarMontaje(2,@m,@v);
 call vehiculosFinalizados(1,@msj,@valorRetorno);
-
-
-
+SELECT @msj, @valor;
+-- -----------------------------------------------------------------------------------
 
